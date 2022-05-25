e7441ef6a49c0083227dc366973294bb7df23664
ssh7937 <91563040+ssh7937@users.noreply.github.com>
2022-05-26 01:40:03 +0900

    식재료 db 과일 채소 쿼리문 수정
    식재료 db에서 식재료 종류가 과일이나 채소일 경우 식재료명을 출력하고 그렇지 않은 경우에는 식재료의 종류를 출력

--- RMS/RMS/app/src/main/java/com/project/rms/Foodlist/Database/ssh_ProductDAO.kt
+++ RMS/RMS/app/src/main/java/com/project/rms/Foodlist/Database/ssh_ProductDAO.kt
@@ -17,6 +17,6 @@ interface ssh_ProductDAO {
     fun getAll(): MutableList<ssh_ProductEntity>
 
     // 유통기한 마감이 임박한 상위 3개 식재료 검색 (이미지 인식한 과일, 채소류는 식재료명을, 그 외 직접 입력, 바코드 인식한 식재료는 분류명이 리스트에 들어가게함)
-    @Query("SELECT CASE WHEN category = '과일' or '채소' THEN name ELSE category END FROM products ORDER BY date")
+    @Query("SELECT CASE WHEN category = '과일' THEN name WHEN category = '채소' THEN name ELSE category END FROM products ORDER BY date")
     fun date(): MutableList<String>
 }
